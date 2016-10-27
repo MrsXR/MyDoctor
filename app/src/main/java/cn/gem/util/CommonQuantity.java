70f57277c8c6e1ac7f40433ddd5650605556b290
sony <627199986@qq.com>
2016-10-27 10:49:41 +0800

    first2

---
+++ app/src/main/java/cn/gem/util/CommonQuantity.java
@@ -0,0 +1,16 @@
+package cn.gem.util;
+
+/**
+ * Created by sony on 2016/10/27.
+ */
+public class CommonQuantity {
+
+    private static final int FIRST=1;
+    private static final int SECOND=2;
+    private static final int THIRD=3;
+    private static final int FOURTH=4;
+    private static final int FIFTH=5;
+    private static final int SIXTH=6;
+
+
+}
