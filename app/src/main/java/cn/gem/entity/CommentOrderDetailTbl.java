9c5e829185fd7801dc88aca21144dd5328d5c09f
sony <627199986@qq.com>
2016-10-29 15:24:30 +0800

    first4

--- app/src/main/java/cn/gem/entity/CommentOrderDetailTbl.java
+++ app/src/main/java/cn/gem/entity/CommentOrderDetailTbl.java
@@ -15,11 +15,72 @@ public class CommentOrderDetailTbl {
 	private int doctorsId;
 	private String commentOrderDetailContent;
 	private int commentOrderDetailType;
+	private int commentOrderDetailAttitude;
+	private int commentOrderDetailTreat;
 	private Timestamp commentOrderDetailTime;
-	
-	
-	
-	
+	private String orderIllSname;
+	private String userSname;
+
+	public CommentOrderDetailTbl(int orderId, int doctorsId, int userId, String commentOrderDetailContent, int commentOrderDetailType, int commentOrderDetailAttitude, int commentOrderDetailTreat, Timestamp commentOrderDetailTime, String orderIllSname, String userSname) {
+		this.orderId = orderId;
+		this.doctorsId = doctorsId;
+		this.userId = userId;
+		this.commentOrderDetailContent = commentOrderDetailContent;
+		this.commentOrderDetailType = commentOrderDetailType;
+		this.commentOrderDetailAttitude = commentOrderDetailAttitude;
+		this.commentOrderDetailTreat = commentOrderDetailTreat;
+		this.commentOrderDetailTime = commentOrderDetailTime;
+		this.orderIllSname = orderIllSname;
+		this.userSname = userSname;
+	}
+
+	public String getUserSname() {
+		return userSname;
+	}
+
+	public void setUserSname(String userSname) {
+		this.userSname = userSname;
+	}
+
+	public String getOrderIllSname() {
+		return orderIllSname;
+	}
+	public void setOrderIllSname(String orderIllSname) {
+		this.orderIllSname = orderIllSname;
+	}
+	public CommentOrderDetailTbl(int commentOrderDetailId, int orderId, int userId, int doctorsId,
+								 String commentOrderDetailContent, int commentOrderDetailType, int commentOrderDetailAttitude,
+								 int commentOrderDetailTreat, Timestamp commentOrderDetailTime) {
+		super();
+		this.commentOrderDetailId = commentOrderDetailId;
+		this.orderId = orderId;
+		this.userId = userId;
+		this.doctorsId = doctorsId;
+		this.commentOrderDetailContent = commentOrderDetailContent;
+		this.commentOrderDetailType = commentOrderDetailType;
+		this.commentOrderDetailAttitude = commentOrderDetailAttitude;
+		this.commentOrderDetailTreat = commentOrderDetailTreat;
+		this.commentOrderDetailTime = commentOrderDetailTime;
+	}
+	public int getDoctorsId() {
+		return doctorsId;
+	}
+	public void setDoctorsId(int doctorsId) {
+		this.doctorsId = doctorsId;
+	}
+	public int getCommentOrderDetailAttitude() {
+		return commentOrderDetailAttitude;
+	}
+	public void setCommentOrderDetailAttitude(int commentOrderDetailAttitude) {
+		this.commentOrderDetailAttitude = commentOrderDetailAttitude;
+	}
+	public int getCommentOrderDetailTreat() {
+		return commentOrderDetailTreat;
+	}
+	public void setCommentOrderDetailTreat(int commentOrderDetailTreat) {
+		this.commentOrderDetailTreat = commentOrderDetailTreat;
+	}
+
 	public Timestamp getCommentOrderDetailTime() {
 		return commentOrderDetailTime;
 	}
@@ -63,7 +124,5 @@ public class CommentOrderDetailTbl {
 		this.commentOrderDetailType = commentOrderDetailType;
 	}
 
-
-	
 
 }
